00ee3fa8de0f37715a032abfbbf99a451ccb00fa
Ricardo Pigeon <ricardopigeon@yahoo.com.mx>
2020-07-30 01:10:30 -0500

    Completed module

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.Enums
+{
+    enum GradeBookType
+    {
+        Standard, 
+        Ranked, 
+        ESNU, 
+        OneToFour, 
+        SixPoint;
+    }
+}
